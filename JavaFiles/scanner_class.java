f8f2326237acfcde4b785e773f4b43338843892a
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2024-09-14 17:40:11 +0530

    scanner added

---
+++ JavaFiles/scanner_class.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// nextInt() =>to read int value from stdin
+// nextLong() =>"...." long"...."
+// nextFloat() =>"...." float"...."
+// nextDouble() =>"...."double"...."
+// next() =>"...." read String value without spaces"...."
+// nextLine() =>"...." read String value with spaces"...."
+
+public class scanner_class {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter len");
+        int l = s.nextInt();
+        System.out.println("Enter breadth");
+        int b = s.nextInt();
+        System.out.println(l * b);
+    }
+}
