ab9a4ef1f04de950ee6fa4cbe9b4221da8f95165
Ganta Srilalitha <srilaitha2003@gmail.com>
2025-09-28 18:06:54 +0530

    Add files via upload
    spark sql basic codes added

---
+++ JavaSpark/JavaSpark/src/test/java/com/sparkcodes/CreateRDDUsingParallelizeTest.java
@@ -0,0 +1,80 @@
+package com.sparkcodes;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CreateRDDUsingParallelizeTest {
+    private final SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("com.sparkcodes.CreateRDDUsingParallelizeTest");
+
+    @Test
+    @DisplayName("Create an empty RDD with no partitions in Spark")
+    public void createAnEmptyRDDWithNoPartitionsInSpark() {
+        try (final var sparkContext = new JavaSparkContext(sparkConf)) {
+            final var emptyRDD = sparkContext.emptyRDD();
+            System.out.println(emptyRDD);
+            System.out.printf("Default number of partitions: %d%n", emptyRDD.getNumPartitions());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Create an empty RDD with default partitions in Spark")
+    public void createAnEmptyRDDWithDefaultPartitionsInSpark() {
+        try (final var sparkContext = new JavaSparkContext(sparkConf)) {
+            final var emptyRDD = sparkContext.parallelize(List.of());
+            System.out.println(emptyRDD);
+            System.out.printf("Default number of partitions: %d%n", emptyRDD.getNumPartitions());
+        }
+
+    }
+
+    @Test
+    @DisplayName("Create a Spark RDD from Java collection using parallelize() method")
+    void createSparkRDDUsingParallelizeMethod() {
+        try (final var sparkContext = new JavaSparkContext(sparkConf)) {
+            final var data = Stream.iterate(1,i->i+1).limit(5).collect(Collectors.toList());
+            final var myRdd = sparkContext.parallelize(data);
+
+            System.out.printf("Total elements in RDD: %d%n", myRdd.count());
+            System.out.printf("Default number of partitions: %d%n", myRdd.getNumPartitions());
+
+            System.out.println("Elements of RDD: ");
+            myRdd.collect().forEach(System.out::println);
+
+            // reduce operations
+            final var max = myRdd.reduce(Integer::max);
+            final var min = myRdd.reduce(Integer::min);
+            final var sum = myRdd.reduce(Integer::sum);
+            System.out.printf("MAX~>%d, MIN~>%d, SUM~>%d%n", max, min, sum);
+        }
+    }
+
+    @Test
+    @DisplayName("Create a Spark RDD from Java collection using parallelize() method with given partitions")
+    void createSparkRDDUsingParallelizeMethodWithGivenPartitions() {
+        try (final var sparkContext = new JavaSparkContext(sparkConf)) {
+            final var data
+                    = Stream.iterate(1, n -> n + 1)
+                    .limit(8)
+                    .collect(Collectors.toList());
+            final var myRdd = sparkContext.parallelize(data, 8);
+            System.out.printf("Total elements in RDD: %d%n", myRdd.count());
+            System.out.printf("Given number of partitions: %d%n", myRdd.getNumPartitions());
+
+            System.out.println("Elements of RDD: ");
+            myRdd.collect().forEach(System.out::println);
+
+            // reduce operations
+            final var max = myRdd.reduce(Integer::max);
+            final var min = myRdd.reduce(Integer::min);
+            final var sum = myRdd.reduce(Integer::sum);
+            System.out.printf("MAX~>%d, MIN~>%d, SUM~>%d%n", max, min, sum);
+        }
+    }
+}
